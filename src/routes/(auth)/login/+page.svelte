fb0408c1c1668fbb5050d0166f676547439316a3
NanezX <victor.nanej@gmail.com>
2025-12-31 11:10:56 -0400

    lint

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -9,18 +9,18 @@
 </svelte:head>
 
 <div
-	class="relative flex min-h-screen items-center justify-center overflow-hidden bg-gradient-to-br from-slate-800 to-slate-900 p-4"
+	class="relative flex min-h-screen items-center justify-center overflow-hidden bg-linear-to-br from-slate-800 to-slate-900 p-4"
 >
 	<!-- Background decoration -->
 	<div class="pointer-events-none absolute inset-0">
 		<div
-			class="absolute -top-[100px] -right-[100px] h-[400px] w-[400px] rounded-full bg-[var(--color-brand-blue)] opacity-40 blur-[80px]"
+			class="absolute -top-[100px] -right-[100px] h-[400px] w-[400px] rounded-full bg-brand-blue opacity-40 blur-[80px]"
 		></div>
 		<div
-			class="absolute -bottom-[50px] -left-[50px] h-[300px] w-[300px] rounded-full bg-[var(--color-brand-yellow)] opacity-40 blur-[80px]"
+			class="absolute -bottom-[50px] -left-[50px] h-[300px] w-[300px] rounded-full bg-brand-yellow opacity-40 blur-[80px]"
 		></div>
 		<div
-			class="absolute top-1/2 left-1/2 h-[200px] w-[200px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[var(--color-brand-blue-light)] opacity-40 blur-[80px]"
+			class="absolute top-1/2 left-1/2 h-[200px] w-[200px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-brand-blue-light opacity-40 blur-[80px]"
 		></div>
 	</div>
 
