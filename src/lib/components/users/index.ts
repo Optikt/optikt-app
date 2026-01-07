9b50670fdcd935138e00907a063a98046264ce36
NanezX <victor.nanej@gmail.com>
2026-01-07 12:58:51 -0400

    remove delete modal because does not exist

--- src/lib/components/users/index.ts
+++ src/lib/components/users/index.ts
@@ -1,6 +1,5 @@
 // Users components barrel export
 export { default as UsersTable } from './UsersTable.svelte';
 export { default as UserFormModal } from './UserFormModal.svelte';
-export { default as DeleteConfirmModal } from './DeleteConfirmModal.svelte';
 export { default as ReactivateConfirmModal } from './ReactivateConfirmModal.svelte';
 export { default as ToggleActiveModal } from './ToggleActiveModal.svelte';
