5c6875f340510490d479a5d0ffd5fc1decffff41
NanezX <victor.nanej@gmail.com>
2026-01-10 13:20:59 -0400

    export compts

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -8,3 +8,6 @@ export { default as FormAlert } from './FormAlert.svelte';
 export { default as SearchInput } from './SearchInput.svelte';
 export { default as TablePagination } from './TablePagination.svelte';
 export { default as ConfirmModal } from './ConfirmModal.svelte';
+export { default as RifInput } from './RifInput.svelte';
+export { default as WhatsAppInput } from './WhatsAppInput.svelte';
+export { default as InstagramInput } from './InstagramInput.svelte';
