a29badd8b92c958db8de498d5e41090efd0c3de5
NanezX <victor.nanej@gmail.com>
2026-01-11 19:23:30 -0400

    entry file for customer components

---
+++ src/lib/components/customers/index.ts
@@ -0,0 +1,3 @@
+export { default as CustomersTable } from './CustomersTable.svelte';
+export { default as CustomerFormModal } from './CustomerFormModal.svelte';
+export { default as CustomerViewModal } from './CustomerViewModal.svelte';
