5c4b741d595d39c264c18d88f11bea77ecea2eb8
NanezX <victor.nanej@gmail.com>
2026-01-11 00:18:03 -0400

    main entry for settings component

---
+++ src/lib/components/config/index.ts
@@ -0,0 +1,4 @@
+// Config components barrel export
+export { default as ProfileSettingsCard } from './ProfileSettingsCard.svelte';
+export { default as ChangePasswordModal } from './ChangePasswordModal.svelte';
+export { default as BusinessSettingsCard } from './BusinessSettingsCard.svelte';
