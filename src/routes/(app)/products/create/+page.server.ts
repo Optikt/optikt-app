c897aff1b002f8d79716260e0f13502b7896f6e3
NanezX <victor.nanej@gmail.com>
2026-01-30 23:14:56 -0400

    usage of queries on page server for data to be used on creation

--- src/routes/(app)/products/create/+page.server.ts
+++ src/routes/(app)/products/create/+page.server.ts
@@ -1,18 +1,18 @@
 import type { PageServerLoad } from './$types';
 import { getAllBrands } from '$lib/server/db/queries/brands';
 import { getAllSuppliers } from '$lib/server/db/queries/suppliers';
-import { getAllLensMaterials } from '$lib/server/db/queries/lenses';
+import { getAllMaterials } from '$lib/server/db/queries/materials';
 
 export const load: PageServerLoad = async () => {
 	const [brands, suppliers, materials] = await Promise.all([
 		getAllBrands(),
 		getAllSuppliers(),
-		getAllLensMaterials()
+		getAllMaterials()
 	]);
 
 	return {
 		brands: brands.map((b) => ({ id: b.id, name: b.name })),
 		suppliers: suppliers.map((s) => ({ id: s.id, name: s.name })),
-		materials: materials.map((m) => ({ id: m.id, name: m.name }))
+		materials: materials.map((m) => ({ id: m.id, name: m.name, productType: m.productType }))
 	};
 };
