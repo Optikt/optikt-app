003cef3984dd76f4de106ded41ce6d9689971ac0
NanezX <victor.nanej@gmail.com>
2026-02-02 16:27:06 -0400

    validate uuids

---
+++ src/lib/utils/uuid.ts
@@ -0,0 +1,9 @@
+/**
+ * UUID utilities
+ */
+
+const UUID_V4_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+
+export function isValidUuid(value: unknown): value is string {
+	return typeof value === 'string' && UUID_V4_REGEX.test(value);
+}
