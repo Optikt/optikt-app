f97607298a01f8cdf87d06c0c5230a6daf4864e0
NanezX <victor.nanej@gmail.com>
2026-02-02 16:28:59 -0400

    validate param id is an UUID

--- src/routes/(app)/products/[id]/+layout.server.ts
+++ src/routes/(app)/products/[id]/+layout.server.ts
@@ -1,12 +1,18 @@
 import type { LayoutServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 import { findProductByIdWithRelations } from '$lib/server/db/queries/products';
+import { isValidUuid } from '$lib/utils/uuid';
 import { getAllBrands } from '$lib/server/db/queries/brands';
 import { getAllSuppliers } from '$lib/server/db/queries/suppliers';
 import { getAllMaterials } from '$lib/server/db/queries/materials';
 import { brands, suppliers, materials } from '$lib/server/db/schema';
 
 export const load: LayoutServerLoad = async ({ params }) => {
+	// Only accept valid UUIDs for product IDs to avoid treating
+	if (!isValidUuid(params.id)) {
+		error(404, 'Producto no encontrado');
+	}
+
 	const product = await findProductByIdWithRelations(params.id);
 
 	if (!product) {
