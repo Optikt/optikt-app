1d26326dcb51e12bca8839c10e8ac1ca8ae42dca
NanezX <victor.nanej@gmail.com>
2026-02-03 18:39:24 -0400

    added custom host

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		allowedHosts: ['elitebook.taild8f0b9.ts.net']
+	},
 
 	test: {
 		expect: { requireAssertions: true },
