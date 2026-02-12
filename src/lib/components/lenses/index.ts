174e25c39578f5438c7a4b43e3b03fc7c8ed1089
NanezX <victor.nanezj@gmail.com>
2026-02-12 19:21:07 -0400

    export all the component fo lenses

---
+++ src/lib/components/lenses/index.ts
@@ -0,0 +1,4 @@
+export { default as LensMaterialsTab } from './LensMaterialsTab.svelte';
+export { default as LensTreatmentsTab } from './LensTreatmentsTab.svelte';
+export { default as LensCatalogTable } from './LensCatalogTable.svelte';
+export { default as LensCatalogForm } from './LensCatalogForm.svelte';
