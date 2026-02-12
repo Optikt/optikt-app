154ec4c0ca586d7665be8be45796d4c458d764f4
NanezX <victor.nanezj@gmail.com>
2026-02-12 16:29:01 -0400

    export command univesal search

--- src/lib/components/layout/index.ts
+++ src/lib/components/layout/index.ts
@@ -1,3 +1,4 @@
 // Layout components
 export { default as Sidebar } from './Sidebar.svelte';
 export { default as NavLink } from './NavLink.svelte';
+export { default as CommandSearch } from './CommandSearch.svelte';
