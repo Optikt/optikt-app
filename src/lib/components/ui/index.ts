20619e19223b47512eee4b7126cba330bf6994b3
NanezX <victor.nanezj@gmail.com>
2026-02-12 23:26:38 -0400

    export entity

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -15,4 +15,4 @@ export { default as InstagramInput } from './InstagramInput.svelte';
 export { default as IdInput } from './IdInput.svelte';
 export { default as CreatableSelect } from './CreatableSelect.svelte';
 export { default as PurchaseCurrencyInput } from './PurchaseCurrencyInput.svelte';
-export type { SelectOption } from './CreatableSelect.svelte';
+export type { SelectOption, PendingEntity } from './CreatableSelect.svelte';
