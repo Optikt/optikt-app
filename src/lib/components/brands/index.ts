1396a537627ccc27d0fd3a935f96e963662a8c6b
NanezX <victor.nanezj@gmail.com>
2026-02-27 23:18:21 -0400

    feat: Add BrandReactivateModal export to brands index

--- src/lib/components/brands/index.ts
+++ src/lib/components/brands/index.ts
@@ -1,3 +1,4 @@
 export { default as BrandsTable } from './BrandsTable.svelte';
 export { default as BrandFormModal } from './BrandFormModal.svelte';
 export { default as BrandViewModal } from './BrandViewModal.svelte';
+export { default as BrandReactivateModal } from './BrandReactivateModal.svelte';
