1c037b003271612e9429a37f43ed7faeeb62e19c
NanezX <victor.nanezj@gmail.com>
2026-03-02 16:25:58 -0400

    feat: Remove ProductFormModal and update exports in products index

--- src/lib/components/products/index.ts
+++ src/lib/components/products/index.ts
@@ -1,5 +1,4 @@
 // Products components barrel export
 export { default as ProductsTable } from './ProductsTable.svelte';
-export { default as ProductFormModal } from './ProductFormModal.svelte';
-export { default as ProductViewModal } from './ProductViewModal.svelte';
 export { default as ProductForm } from './ProductForm.svelte';
+export { default as ProductReactivateModal } from './ProductReactivateModal.svelte';
