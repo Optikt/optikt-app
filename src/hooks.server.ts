84d9ae534e37c4dd2918a1353f4436319acef30b
NanezX <victor.nanezj@gmail.com>
2026-04-21 14:17:45 -0400

    feat: add error handling to hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleServerError } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth';
 
 const handleAuth: Handle = async ({ event, resolve }) => {
@@ -23,6 +23,13 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	event.locals.session = session;
 
 	return resolve(event);
+};
+
+export const handleError: HandleServerError = ({ error }) => {
+    const err = error as { message?: string; cause?: unknown };
+    console.error('[handleError]', err?.message);
+    if (err?.cause) console.error('[handleError cause]', err.cause);
+    return { message: 'Internal Error' };
 };
 
 export const handle: Handle = handleAuth;
