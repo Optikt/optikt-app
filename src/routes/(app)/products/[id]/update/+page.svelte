8b288c51ec0fc909c9b40a830777f929a0ee4390
NanezX <victor.nanezj@gmail.com>
2026-04-22 11:15:57 -0400

    Remove max-width constraint from product update container

--- src/routes/(app)/products/[id]/update/+page.svelte
+++ src/routes/(app)/products/[id]/update/+page.svelte
@@ -13,7 +13,7 @@
 <svelte:head>
 	<title>Editar {product.name} - Optikt</title>
 </svelte:head>
-<div class="mx-auto max-w-6xl px-4 py-6 sm:px-6 lg:px-8">
+<div class="mx-auto px-4 py-6 sm:px-6 lg:px-8">
 	<PageHeader
 		title="Editar Producto"
 		subtitle={product.sku}
