988a1d42471a8cf5f742505bde6a320ef7d5da7c
NanezX <victor.nanezj@gmail.com>
2026-04-23 10:21:26 -0400

    backto

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/sveltekit';
 import { env } from '$env/dynamic/public';
 import type { HandleClientError } from '@sveltejs/kit';
-import { captureClientException, reportClientError } from '$lib/utils';
+import { reportClientError } from '$lib/utils';
 
 const sentryDsn = env.PUBLIC_SENTRY_DSN;
 
@@ -13,23 +13,14 @@ if (sentryDsn) {
 	});
 }
 
-// const clientHandleError: HandleClientError = ({ error, event, status, message }) => {
-// 	reportClientError('hooks.client', error, {
-// 		status,
-// 		message,
-// 		url: event.url.href
-// 	});
-
-// 	return { message };
-// };
-
 const clientHandleError: HandleClientError = ({ error, event, status, message }) => {
-    captureClientException('hooks.client', error, { // envía a Sentry
-        status,
-        message,
-        url: event.url.href
-    });
-    return { message };
+	reportClientError('hooks.client', error, {
+		status,
+		message,
+		url: event.url.href
+	});
+
+	return { message };
 };
 
 export const handleError = sentryDsn
